99d86a7571965197f2b4e6b697d17ca6c14a21dd
aslambaba <42738148+aslambaba@users.noreply.github.com>
2020-11-10 13:56:14 +0500

    Add Boiler Plate And Fetch API Using MirageJS

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+import React , { useEffect } from 'react';
+
+function App() {
+
+  useEffect(()=>{
+    async function getAPI(){
+      const data = await fetch('/api/movies');
+      const result = await data.json();
+      console.log(result);
+    };
+    getAPI();
+  },[])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
